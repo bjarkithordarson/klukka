e573ebe2d638517322bee90495b9fabad2a1c00c
Bjarki Rafn Þórðarson <4554182+bjarkithordarson@users.noreply.github.com>
2019-04-09 11:53:08 +0000

    Background fix

--- prevent-burn-in.js
+++ prevent-burn-in.js
@@ -3,8 +3,8 @@
     const preventBurnIn = function() {
         let x = Math.floor(Math.random() * (50-(-50)+1) + (-50)),
             y = Math.floor(Math.random() * (50-(-50)+1) + (-50)),
-            scale = Math.random() * (1-0.95) + 0.95;
-        container.style.transform = "translate(" + x + "px, " + y + "px) scale("+scale+")";
+            //scale = Math.random() * (1-0.95) + 0.95;
+        container.style.transform = "translate(" + x + "px, " + y + "px)";
 
         setTimeout(preventBurnIn, Math.random() * ((1000*60*10)-(1000*60*5)+1)+(1000*60*5));
     }
